9bce2148680aad3cd31a876fc4ae3e8e69b3edf1
SolaTukishiro <antaku0514@icloud.com>
2026-03-01 12:52:00 +0900

    波線対応

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -50,6 +50,7 @@ class AuthController extends Controller
             ]);
         }
 
+        /** @var \App\Models\User $user */
         $user  = Auth::user();
         $token = $user->createToken('auth_token')->plainTextToken;
 
