dc2a5d5ef82751cb41bef24c29a4767121ae213d
SolaTukishiro <antaku0514@icloud.com>
2026-03-20 18:24:35 +0900

    dashboard

---
+++ frontend/src/api/projects.ts
@@ -0,0 +1,21 @@
+import client from './client';
+import type { Project } from '../types/project';
+
+export const fetchProjects = async (): Promise<Project[]> => {
+  const res = await client.get('/projects');
+  return res.data.data;
+};
+
+export const createProject = async (data: Partial<Project>): Promise<Project> => {
+  const res = await client.post('/projects', data);
+  return res.data.data;
+};
+
+export const updateProject = async (id: number, data: Partial<Project>): Promise<Project> => {
+  const res = await client.put(`/projects/${id}`, data);
+  return res.data.data;
+};
+
+export const deleteProject = async (id: number): Promise<void> => {
+  await client.delete(`/projects/${id}`);
+};
